2ecd39ed9d9ce871c0234551e646e890ff79b27b
seyyed ali hossein nezhad <alihosseinnezhad3@gmail.com>
2022-07-09 13:38:29 +0430

    Create RepeatModeUseCase.kt

---
+++ app/src/main/java/com/gamapp/dmplayer/framework/player/usecase/RepeatModeUseCase.kt
@@ -0,0 +1,9 @@
+package com.gamapp.dmplayer.framework.player.usecase
+
+import com.gamapp.domain.usecase.player.RepeatModeUseCase
+
+class RepeatModeUseCaseImpl:RepeatModeUseCase {
+    override suspend fun invoke() {
+        TODO("Not yet implemented")
+    }
+}
